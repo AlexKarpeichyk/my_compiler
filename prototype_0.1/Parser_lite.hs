313463ab60b1a99088cc4319a2346383917d9076
Xander <alex.karp475@gmail.com>
2019-03-01 15:35:26 +0000

    Parser_lite updated

--- prototype_0.1/Parser_lite.hs
+++ prototype_0.1/Parser_lite.hs
@@ -4,15 +4,41 @@ import Lexer
 
 -- Datatypes
 
-data BLOCK = Block (ENE) deriving (Show)
-data ENE = E | Seq (E) (ENE) derivign (Show)
-data E = INT_P Integer | BLOCK | SKIP deriving (Show, Eq)
+data BLOCK = Block (ENE) deriving (Show, Eq)
+data ENE = Single (E) | Seq (E) (ENE) deriving (Show, Eq)
+data E = INT_P Integer | Nest (BLOCK) | SKIP deriving (Show, Eq)
 
 isInt (INT x) = True
 isInt _ = False
 
+seqLeft l (h:t)
+  | h /= Semicolon = seqLeft (h:l) t
+  | otherwise = l
+
+seqRight (h:t)
+  | h /= Semicolon = seqRight t
+  | otherwise = t
+
+parse :: [Token] -> [Token] -> BLOCK
+parse expec (h:t)
+--  | not (elem h [LCurlyBracket, RCurlyBracket]) = parseENE expec (h:t)
+  | h == LCurlyBracket = Block (parseENE (RCurlyBracket:expec) t)
+  | h == RCurlyBracket && length expec /= 0 = parse (tail expec) t
+  | otherwise = error "Parse error: not a block."
+
+parseENE :: [Token] -> [Token] -> ENE
+parseENE expec (h:t)
+  | elem Semicolon (h:t) = parseSeq expec (h:t)
+  | otherwise = Single (parseE expec (h:t))
+
+parseSeq :: [Token] -> [Token] -> ENE
+parseSeq expec (h:t) = Seq (parseE expec (seqLeft [] (h:t))) (parseENE expec (seqRight (h:t)))
+
+parseE :: [Token] -> [Token] -> E
+parseE expec (h:t)
+  | isInt h = parseInt h 
+  | h == Skip = SKIP
+  | otherwise = Nest (parse expec (h:t))
+
 parseInt :: Token -> E
 parseInt (INT x) = INT_P x
-
-parseBlock (LCurlyBracket:t)
-  |  
